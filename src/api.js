3721eb59a902bc6fbaf9746eecdbac93a9d8b16f
doctype-Melvin <doctype.melvin@gmail.com>
2022-09-06 15:14:38 +0200

    adds 404 obj message to result

--- src/api.js
+++ src/api.js
@@ -7,16 +7,25 @@ function getWeather(value){
     const wind = document.getElementById('wind');
     const perc = document.getElementById('perc');
 
+    function clearResults(){
+        temp.textContent = '';
+        wind.textContent = '';
+        perc.textContent = '';
+    }
+
     return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${value}&appid=11558c3112856939631bac223b8cddb5&units=metric`, {mode: 'cors'})
     .then(function(response) {
         return response.json()
     })
     .then(function(response){
         console.log(response)
-        city.textContent = response.name;
+        city.textContent = response.name || response.message;
         temp.textContent = `Temperature: ${response.main.temp} Celcius`;
         wind.textContent = `Wind speed: ${response.wind.speed} kmh`;
         perc.textContent = `Percipitation: ${response.weather[0].description}`
+    })
+    .catch(function(){
+        clearResults();
     });
 }
 btn.addEventListener('click', (e) => {
