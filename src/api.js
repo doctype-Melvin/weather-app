97761ba718c92a97bada68fbe3d6213ba26adbc9
doctype-Melvin <doctype.melvin@gmail.com>
2022-09-06 14:59:00 +0200

    adds weather details to display section

--- src/api.js
+++ src/api.js
@@ -1,18 +1,26 @@
-const search = document.getElementById('search')
 const display = document.querySelector('.display');
-const btn = document.getElementById('btn')
+const btn = document.getElementById('btn');
 
 function getWeather(value){
+    const city = document.getElementById('city');
+    const temp = document.getElementById('temp');
+    const wind = document.getElementById('wind');
+    const perc = document.getElementById('perc');
+
     return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${value}&appid=11558c3112856939631bac223b8cddb5&units=metric`, {mode: 'cors'})
-.then(function(response) {
-    return response.json()
-})
-.then(function(response){
-    console.log(response)
-    display.textContent = response.main.temp
-});
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(response){
+        console.log(response)
+        city.textContent = response.name;
+        temp.textContent = `Temperature: ${response.main.temp} Celcius`;
+        wind.textContent = `Wind speed: ${response.wind.speed} kmh`;
+        perc.textContent = `Percipitation: ${response.weather[0].description}`
+    });
 }
 btn.addEventListener('click', (e) => {
+    const search = document.getElementById('search')
     console.log(search.value);
     getWeather(search.value)
     e.preventDefault()
