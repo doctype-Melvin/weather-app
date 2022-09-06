46e1b2b57a8cb867853998dbe50412af3c832e40
doctype-Melvin <doctype.melvin@gmail.com>
2022-09-06 14:46:39 +0200

    adds basic search and display of temperature in desired city

--- src/api.js
+++ src/api.js
@@ -1,6 +1,9 @@
+const search = document.getElementById('search')
 const display = document.querySelector('.display');
+const btn = document.getElementById('btn')
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=hamburg&appid=11558c3112856939631bac223b8cddb5&units=metric', {mode: 'cors'})
+function getWeather(value){
+    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${value}&appid=11558c3112856939631bac223b8cddb5&units=metric`, {mode: 'cors'})
 .then(function(response) {
     return response.json()
 })
@@ -8,3 +11,9 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=hamburg&appid=11558c3112
     console.log(response)
     display.textContent = response.main.temp
 });
+}
+btn.addEventListener('click', (e) => {
+    console.log(search.value);
+    getWeather(search.value)
+    e.preventDefault()
+})
